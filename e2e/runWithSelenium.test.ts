37445c25249ecf833d8a5c862c6d96d0214f135d
Tom Sacher <tom.sacher@rnd.de>
2020-12-07 19:21:56 +0100

    debug maybe

--- e2e/runWithSelenium.test.ts
+++ e2e/runWithSelenium.test.ts
@@ -25,32 +25,9 @@ beforeAll(async () => {
         "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
       );
       process.env.PATH = `${process.env.PATH};${driverPath};`;
-      console.log(driverPath);
       capabilities = webdriver.Capabilities.ie();
       capabilities.set("ignoreProtectedModeSettings", true);
       capabilities.set("ignoreZoomSetting", true);
-      break;
-    }
-    case "safari": {
-      capabilities = webdriver.Capabilities.safari();
-      break;
-    }
-    case "firefox": {
-      require("geckodriver");
-      capabilities = webdriver.Capabilities.firefox();
-      break;
-    }
-    case "chrome": {
-      require("chromedriver");
-      capabilities = webdriver.Capabilities.chrome();
-      capabilities.set("chromeOptions", {
-        args: [
-          "--headless",
-          "--no-sandbox",
-          "--disable-gpu",
-          "--window-size=1980,1200"
-        ]
-      });
       break;
     }
   }
